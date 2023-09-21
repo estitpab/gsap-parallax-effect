97db4e849f642099cf1b06994bd5e756ed588ee5
estitpab <estitpab@gmail.com>
2023-09-21 10:10:46 +0200

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,32 @@ export default function Scroll() {
         <ParallaxItem>
           <p>It's amazing</p>
         </ParallaxItem>
+        <ParallaxItem>
+          <div style={{ textAlign: 'center' }}>
+            <h2
+              style={{
+                color: 'white',
+                background: 'black',
+                display: 'inline-block',
+                padding: '10px',
+                borderRadius: '10px',
+              }}
+            >
+              What a title bloc !
+            </h2>
+            <p style={{ margin: '20px 0' }}>
+              A paragraph very interesting about lot of things
+            </p>
+            <img
+              style={{ borderRadius: '30px' }}
+              src="https://images.unsplash.com/photo-1693922876041-7fb22341f33a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3465&q=80"
+              width="200"
+            />
+          </div>
+        </ParallaxItem>
+        <ParallaxItem>
+          <p>Thank you for watching</p>
+        </ParallaxItem>
       </ParallaxWrapper>
     </div>
   );
